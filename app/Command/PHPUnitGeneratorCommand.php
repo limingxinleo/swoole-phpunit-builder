e54313f4b6631507718e4085f6af97170d564859
李铭昕 <715557344@qq.com>
2021-03-26 17:10:07 +0800

    Added Github Actions for build phpunit

---
+++ app/Command/PHPUnitGeneratorCommand.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace App\Command;
+
+use Hyperf\Command\Annotation\Command;
+use Hyperf\Command\Command as HyperfCommand;
+use Psr\Container\ContainerInterface;
+
+/**
+ * @Command
+ */
+class PHPUnitGeneratorCommand extends HyperfCommand
+{
+    /**
+     * @var ContainerInterface
+     */
+    protected $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+
+        parent::__construct('gen:phpunit');
+    }
+
+    public function configure()
+    {
+        parent::configure();
+        $this->setDescription('Generate PHPUnit');
+    }
+
+    public function handle()
+    {
+
+    }
+}
